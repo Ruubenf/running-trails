4ff3ed06d7d246b154fdb2946c2e34e4bde84de9
Ruubenf <rfemenia2001@gmail.com>
2025-02-07 12:56:39 +0000

    Added get comments method

--- api/api.py
+++ api/api.py
@@ -1,6 +1,4 @@
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
@@ -28,66 +26,8 @@ def get_db_connection():
         cursor_factory=RealDictCursor
     )
 
-"""
-COMMENTS SAVED FOR FUTURE WORK
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
-db = SQLAlchemy(app)
-api = Api(app)
-class TrailModel(db.Model):
-    __tablename__ = 'trail'
-    __table_args__ = {"schema": "sa"}
-    id_0 = db.Column(db.Integer, primary_key=True, auto_increment=True)
-    geom = db.Column(db.String)
-    name = db.Column(db.String(255))
-    descript = db.Column(db.String(255))
-    location = db.Column(db.String(255))
-    type_terra = db.Column(db.String(255))
-    slope_mean = db.Column(db.BigInteger)
-    slope_max = db.Column(db.BigInteger)
-    distance_m = db.Column(db.Double)
-
-    def __repr__(self):
-        return f'<Trail {self.name}, id: {self.id_0}>'
-
-
-
-
-trail_args = reqparse.RequestParser()
-trail_args.add_argument('name', type=str, help='Name of the trail')
-trail_args.add_argument('geom', type=str, help='Geometry of the trail')
-trail_args.add_argument('descript', type=str, help='Description of the trail')
-trail_args.add_argument('location', type=str, help='Location of the trail')
-trail_args.add_argument('type_terra', type=str, help='Type of terrain')
-trail_args.add_argument('slope_mean', type=int, help='Mean slope of the trail')
-trail_args.add_argument('slope_max', type=int, help='Max slope of the trail')
-trail_args.add_argument('distance_m', type=float, help='Distance of the trail')
-
-trailFields = {
-    'id_0': fields.Integer,
-    'geom': fields.String,
-    'name': fields.String,
-    'descript': fields.String,
-    'location': fields.String,
-    'type_terra': fields.String,
-    'slope_mean': fields.Integer,
-    'slope_max': fields.Integer,
-    'distance_m': fields.Float
-}
-
-class Trail(Resource):
-    def get(self):
-        trails = TrailModel.query.all()
-        return trails
-
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-"""
-
-
-# GET all rides from pa.rides_geojson
+# GET all trail from sa.trail
 @app.route('/trails', methods=['GET'])
 def get_rides():
     conn = get_db_connection()
@@ -98,6 +38,16 @@ def get_rides():
     conn.close()
     return jsonify(rides)
 
+# GET comments for a specific trail
+@app.route('/trail/<int:id_trail>/comments', methods=['GET'])
+def get_comments(id_trail):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(f"""SELECT * FROM sa.comment WHERE id_trail = {id_trail}""")
+    comments = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(comments)
 
 
 if __name__ == '__main__':
