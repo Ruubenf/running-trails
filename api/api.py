08d2f2e6e0b1d865429c9e7c89f39f890a444cf9
Ruubenf <rfemenia2001@gmail.com>
2025-02-08 00:30:49 +0000

    Added get get trail distance endpoint

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import requests
 
 
 DB_CONFIG = {
@@ -96,6 +97,57 @@ def get_difficulty_trails(difficulty):
     conn.close()
     return jsonify(rides)
 
+
+# Get trails by location
+@app.route('/trails/location', methods=['GET'])
+def get_location_trails():
+
+    long = request.args.get('long')
+    lat = request.args.get('lat')
+    epsg = request.args.get('epsg')
+
+    # Validate arguments
+    if not long or not lat or not epsg:
+        return jsonify({"error": "Missing arguments"})
+    if epsg not in ["4326", "3763"]:
+        return jsonify({"error": "Invalid EPSG"})
+    
+
+    point = ""
+    if epsg == "4326":
+        point = f"ST_TRANSFORM(ST_GEOMFROMTEXT('POINT({long} {lat})', 4326), 3763"
+    elif epsg == "3763":
+        point = f"ST_GEOMFROMTEXT('POINT({long} {lat})', 3763)"
+
+
+    """
+
+    # Get desired location coords with https://nominatim.openstreetmap.org/search?q=valencia&format=json&countrycodes=pt
+    loc = requests.get(f"https://nominatim.openstreetmap.org/search?q={location}&format=json&countrycodes=pt")
+    print(loc.content)
+
+    loc = loc.content.json()[0]
+
+    lon = loc["lon"] #"-9.1646135"
+    lat = loc["lat"] #"38.727895"
+    """
+    # select st_x(st_transform(ST_GEOMFROMTEXT('POINT(38.72 -9.16)', 4326), 3763))
+
+    """select st_x(st_centroid(st_transform(geom, 3763))), st_y(st_centroid(st_transform(geom, 3763)))
+    from sa.trail"""
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(f"""
+    select id_0, name, 
+    cast(st_distance({point}, st_centroid(st_transform(geom, 3763))) as numeric(10,2)) as dist
+    from sa.trail
+    order by dist asc
+    """)
+    rides = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(rides)
 
 
 if __name__ == '__main__':
