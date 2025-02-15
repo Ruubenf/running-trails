f38c594e81686b70838cda28ff40cf0e0f353af5
Ruubenf <rfemenia2001@gmail.com>
2025-02-15 23:10:23 +0000

    Calculate terrain type trigger

--- db/04-create_triggers.sql
+++ db/04-create_triggers.sql
@@ -1,3 +1,4 @@
+-- Calculate green area distance on the newly added trail
 CREATE OR REPLACE FUNCTION tg_add_trail()
 RETURNS TRIGGER 
 LANGUAGE PLPGSQL
@@ -25,3 +26,36 @@ BEFORE INSERT
 ON sa.trail
 FOR EACH ROW
 EXECUTE PROCEDURE tg_add_trail();
+
+-- Calculate the type of terrain in the newly added trail
+CREATE OR REPLACE FUNCTION tg_add_pav()
+RETURNS TRIGGER 
+LANGUAGE PLPGSQL
+AS $$
+
+DECLARE 
+	pav_ratio VARCHAR(155);
+BEGIN
+    
+	SELECT 
+		CASE
+			WHEN SUM(ST_LENGTH(ST_INTERSECTION(NEW.geom, bga.geom))) / NEW.distance_m < 0.3 THEN 'Dirt/Gravel/Unknown'
+			ELSE 'Pavimented'
+		END INTO pav_ratio
+	FROM (
+		SELECT ST_UNION(ST_BUFFER(ST_TRANSFORM(pav.geom, 3763), 20)) AS geom 
+		FROM sa.footway AS pav 
+		WHERE ST_INTERSECTS(NEW.geom, ST_BUFFER(ST_TRANSFORM(pav.geom, 3763), 20))
+	) AS bga;
+	
+	NEW.type_terra := pav_ratio;
+
+	RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE TRIGGER trigger_add_pav
+BEFORE INSERT 
+ON sa.trail
+FOR EACH ROW
+EXECUTE PROCEDURE tg_add_pav();
