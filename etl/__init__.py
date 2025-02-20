cf2b1de8fb238f401445303d07c04ff80c33602f
Ruubenf <rfemenia2001@gmail.com>
2025-02-20 10:46:10 +0000

    Added get_trails api connection

--- etl/__init__.py
+++ etl/__init__.py
@@ -1,4 +1,5 @@
 from logs import init_logger
+from get_trails import get_trails
 from kml_to_shp import init_converter
 from modify_shp import modify_columns
 from shp_to_db import saveshp_to_db, del_folders
@@ -6,6 +7,8 @@ from footway_to_db import savefootway_to_db
 from ga_to_db import save_ga_to_db
 
 init_logger()
+
+get_trails()
 
 init_converter()
 
---
+++ etl/get_trails.py
@@ -0,0 +1,27 @@
+import requests
+import os
+
+def get_trails():
+    # API base url
+    base_url = "https://trails.rfemenia.com/get_trails.php"
+    download_url = "https://trails.rfemenia.com/trails_dir/"
+
+    # Dir where to download kml files
+    output_dir = "etl/data/original/"
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Get KML file list
+    response = requests.get(base_url)
+    if response.status_code == 200:
+        kml_files = response.json()
+        for kml_file in kml_files:
+            file_url = download_url + kml_file
+            file_response = requests.get(file_url)
+            if file_response.status_code == 200:
+                with open(os.path.join(output_dir, kml_file), "wb") as f:
+                    f.write(file_response.content)
+                print(f"✅ Downloading: {kml_file}")
+            else:
+                print(f"❌ Failure to download {kml_file}")
+    else:
+        print("❌ File list could not be found on the api")
